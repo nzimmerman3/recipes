4a062c726c87bb9dbead97ac447038717b41df7f
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2022-03-10 22:46:26 -0800

    Created form and API calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+import axios from "axios"
+import Food from './components/Food'
+
+const fetchRecipes = (searchFood, setFoods) => {
+  let searchUrl = "https://nutritionix-api.p.rapidapi.com/v1_1/search/" + searchFood
+  var options = {
+    method: 'GET',
+    url: searchUrl,
+    params: { fields: 'item_name,item_id,brand_name,nf_calories,nf_total_fat' },
+    headers: {
+      'x-rapidapi-host': 'nutritionix-api.p.rapidapi.com',
+      'x-rapidapi-key': '8c5ea2e881msh5adc46f47dbb2acp1c24e0jsn6a9427c7a103'
+    }
+  };
+
+  axios.request(options).then(response => {
+    // console.log(response["data"]["hits"]);
+    setFoods(response["data"]["hits"])
+    return response
+  })
+}
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [foods, setFoods] = useState([])
+
+  // useEffect(() => {
+  //   fetchRecipes(food)
+  // }, [food])
+  console.log(foods)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Food:
+          <input type="text" value={search} onChange={(event) => setSearch(event.target.value)}></input>
+          <button type="button" onClick={() => fetchRecipes(search, setFoods)}>Search</button>
+        </label>
+      </form>
+      {foods.map(food => {
+        return (<Food disp={food} />)
+      })}
     </div>
   );
 }
